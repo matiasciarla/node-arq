b7001e04952199e72a59e60ddb5af8a4e2600920
Ciarla, Matias <matias.ciarla@opendevsolutions.com.ar>
2018-08-22 12:15:06 -0300

    desacople de vista

--- ts/server/scripts/create.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS clients;
-
-create table clients (
-    id int unsigned auto_increment primary key,
-    first_name varchar(50), 
-    last_name varchar(50)
-) ENGINE=INNODB;
-
- 
